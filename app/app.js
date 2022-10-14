cead20829455ce315301a5de40ed1d4b48146fa4
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-10-14 14:35:30 -0400

    Movies List Page completed

--- app/app.js
+++ app/app.js
@@ -1,57 +1,56 @@
-// Third Party Modules
-import express from "express";
-import cookieParser from "cookie-parser";
+// Third-Party Modules
+import express from 'express';
+import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import session from "express-session";
+import session from 'express-session';
 
-// ES Modules fix for __dirname 
-import path, {dirname} from 'path';
+// ES Modules fix for __dirname
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Import Mongoose Module
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 // Configuration Module
-import { MongoURI, Secret } from "../config/config.js";
+import { MongoURI, Secret } from '../config/config.js';
 
-// Import Router
-import indexRouter from './routes/index.route.server.js';
+// Import Routes
+import indexRouter from './routes/index.route.server.js'
+import movieRouter from './routes/movies.route.server.js';
 
-// instantiate app-server
+// Instantiate Express Application
 const app = express();
 
-// Complete the DB configuration
+// Complete the DB Configuration
 mongoose.connect(MongoURI);
 const db = mongoose.connection;
 
-// Listen for connection success or error
+//Listen for connection success or error
 db.on('open', () => console.log("Connected to MongoDB"));
 db.on('error', () => console.log("Mongo Connection Error"));
 
-// Set up Middlewares
+// Set Up Middlewares
 
-// setup ViewEngine EJS
-app.set('views', path.join(__dirname,'/views'));
+// Setup ViewEngine EJS
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev')); 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname,'/client')));
+app.use(express.static(path.join(__dirname,'../public')));
 app.use(session({
     secret: Secret,
-    saveUninitialized: false,
+    saveUninitialized: false, 
     resave: false
 }));
 
 // Use Routes
 app.use('/', indexRouter);
+app.use('/', movieRouter);
 
-// // run app
-// app.listen(3000);
-
-// console.log('Server running at http://localhost:3000');
 
 export default app;
